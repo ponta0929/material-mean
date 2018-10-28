63b32d34e0c4da9f54c231085acd9c9f981fe76d
ponta0929 <ponta.matumoto2@gmail.com>
2018-10-29 00:14:54 +0900

    最低限の基礎部分を仮作成

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { TempComponent } from "./material/temp/temp.component";
+
+const routes: Routes = [
+  { path : "", redirectTo : "/temp", pathMatch : "full" },
+  { path : "temp", component : TempComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TempComponent } from './material/temp/temp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TempComponent
   ],
   imports: [
     BrowserModule,
